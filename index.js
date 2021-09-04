61ed3ca303d7f92b607c6a27a22f84035f52372c
madloyer <87596107+madloyer@users.noreply.github.com>
2021-09-04 14:44:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -83,6 +83,7 @@ const url4 = url2[1];
 // inline keyboard
 const inKey = [
   [{text: `${url3}`, url: `${url4}`}]
+  [{text:'Join Channel',callback_data:'DOC'},{text:' Join Vip',callback_data:'HELP'}],
 ];
 
 const inKey2 = [
