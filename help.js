ec46dd673d80200b5fae45a6b866af4a3fadb689
madloyer <87596107+madloyer@users.noreply.github.com>
2021-09-04 14:13:35 +0700

    Create help.js

---
+++ help.js
@@ -0,0 +1,39 @@
+module.exports.bothelp = `Yoo, Guwe botnya Mantapjozz Channel. Untuk Join Group Vip PPV silahkan cek list dibawah.`;
+
+module.exports.botcommand = `<u>Berikut adalah beberapa perintah dan penggunaan admin.</u>
+\n\n<b>Kegunaan Nomor US untuk agar dapat mengakses Group/Channel Yang di banned dari Nomor indo.</b>
+\n<b>Untuk pembelian bisa langsung Chat admin dibawah va Wa :</b>
+\nadmin 1 : <a href="http://wa.me/380995437384">Softmilk</a>
+\nadmin 2 : <a href="http://wa.me/84589340445">Amadeus</a>
+\n\nKalau gak mau beli atau mau gratisan bisa juga langsung bikin sendiri dengan gunain APK 2NDLINE\nBisa download di Playstore ataupun Googling aja dan untuk Tutorial bisa cek YT admin dibawah:
+\n<a href="https://youtu.be/920nA1E8ON8">Tutorial Bikin Nomor US (2ndline)</a>
+\nIkuti tutorialnya yang benar biar bisa langsung dapet dan jangan nyerah jika gagal #tetapcoliwalaugakberguna
+\n\nUntuk Contoh testi atau contoh group yang ke banned nomor indo sehingga butuh akses Nomor Us bisa kalian kepo di group dibawah ini:
+\n<a href="https://t.me/testimonijasanous/28">Testimoni Mantapjozz</a>
+`;
+
+module.exports.botinstall = `<u>List Join Group Vip PPV / Full Genre Indo, asian, West.</u>
+\nJoin Vip Gudang Bokep
+\nGroup Vip Permanent
+\n==================
+\nGroup Full Indo : 35k
+\nGroup Full Asian : 35k
+\nGroup Full West : 35k
+\nGroup Vip PPV  : 50k
+\nNomor Luar / US : 35k
+\n==================
+\nGroup Full Indo, Asian , West : 100K
+\nGroup VIP PPV +  Nomor US = 75K
+\n==================
+\nGroup Full + PPV + Nomor Luar Negeri = 170K
+\n==================
+\ninfo : untuk Group PPV Media 150k ++ dan butuh Nomor selain +62 untuk akses.
+\n================
+\nnote : 
+\n- untuk sementara vip gudang bokep masih bisa menggunakan no indo
+\n- pembayaran via ovo, dana, gopay 
+\n================
+\nuntuk join silahkan chat admin👇
+\nadmin 1 : <a href="http://wa.me/380995437384">Softmilk</a>
+\nadmin 2 : <a href="http://wa.me/84589340445">Amadeus</a> .
+`;
