edbc2e7c47a6de54c663710cb5a54c338700d98d
madloyer <87596107+madloyer@users.noreply.github.com>
2021-10-09 04:23:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -115,7 +115,7 @@ const inKey = [
 
 const inKey2 = [
     [{text: `${url3}`, url: `${url4}`}],
-    [{text:`Get File`, url: `https://t.me/${process.env.BOTUSERNAME}?start=grp_${ctx.message.media_group_id}`]
+    [{text:`Get File`, url: `https://t.me/${process.env.BOTUSERNAME}?start=grp_${ctx.message.media_group_id}`}]
 ];
 
 //BOT START
