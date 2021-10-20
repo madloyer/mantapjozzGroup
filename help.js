7c638564b98bd06cec6380ce121bae023cdbca11
madloyer <87596107+madloyer@users.noreply.github.com>
2021-10-20 15:34:18 +0700

    Update help.js

--- help.js
+++ help.js
@@ -1,49 +1,49 @@
 module.exports.bothelp = `Yoo, Guwe botnya Mantapjozz Channel. Untuk Join Group Vip PPV silahkan cek list dibawah.`;
 
 module.exports.botcommand = `<u>Berikut adalah beberapa perintah dan penggunaan admin.</u>
-\n\n<b>Kegunaan Nomor US untuk agar dapat mengakses Group/Channel Yang di banned dari Nomor indo.</b>
-\n<b>Untuk pembelian bisa langsung Chat admin dibawah va Wa :</b>
-\nadmin 1 : <a href="http://wa.me/380995437384">Softmilk</a>
-\nadmin 2 : <a href="http://wa.me/84589340445">Amadeus</a>
-\n\nKalau gak mau beli atau mau gratisan bisa juga langsung bikin sendiri dengan gunain APK 2NDLINE\nBisa download di Playstore ataupun Googling aja dan untuk Tutorial bisa cek YT admin dibawah:
-\n<a href="https://youtu.be/920nA1E8ON8">Tutorial Bikin Nomor US (2ndline)</a>
-\nIkuti tutorialnya yang benar biar bisa langsung dapet dan jangan nyerah jika gagal #tetapcoliwalaugakberguna
-\n\nUntuk Contoh testi atau contoh group yang ke banned nomor indo sehingga butuh akses Nomor Us bisa kalian kepo di group dibawah ini:
-\n<a href="https://t.me/testimonijasanous/28">Testimoni Mantapjozz</a>
+\n<b>Kegunaan Nomor US untuk agar dapat mengakses Group/Channel Yang di banned dari Nomor indo.</b>
+<b>Untuk pembelian bisa langsung Chat admin dibawah va Wa :</b>
+admin 1 : <a href="http://wa.me/380995437384">Softmilk</a>
+admin 2 : <a href="http://wa.me/84589340445">Amadeus</a>
+\nKalau gak mau beli atau mau gratisan bisa juga langsung bikin sendiri dengan gunain APK 2NDLINE\nBisa download di Playstore ataupun Googling aja dan untuk Tutorial bisa cek YT admin dibawah:
+<a href="https://youtu.be/920nA1E8ON8">Tutorial Bikin Nomor US (2ndline)</a>
+Ikuti tutorialnya yang benar biar bisa langsung dapet dan jangan nyerah jika gagal #tetapcoliwalaugakberguna
+\nUntuk Contoh testi atau contoh group yang ke banned nomor indo sehingga butuh akses Nomor Us bisa kalian kepo di group dibawah ini:
+<a href="https://t.me/testimonijasanous/28">Testimoni Mantapjozz</a>
 `;
 
 module.exports.botinstall = `<u>List Join Group Vip PPV / Full Genre Indo, asian, West.</u>
-\nJoin Vip Gudang Bokep
-\nGroup Vip Permanent
-\n==================
-\nGroup Full Indo : 50k
+Join Vip Gudang Bokep
+Group Vip Permanent
+==================
+Group Full Indo : 50k
 Group Full Asian : 50k
 Group Full West : 50k
 Group Vip PPV  : 65k
 \nNomor Luar / US : 45k
 ==================
-\nGroup Full Indo, Asian , West : 200K
+Group Full Indo, Asian , West : 200K
 Group VIP PPV +  Nomor US = 100K
 ==================
-\nGroup Full + PPV + Nomor Luar Negeri = 250K
+Group Full + PPV + Nomor Luar Negeri = 250K
 ==================
-\ninfo : untuk Group PPV Media 250k ++ dan butuh Nomor selain +62 untuk akses.
+info : untuk Group PPV Media 250k ++ dan butuh Nomor selain +62 untuk akses.
 ================
-\nnote : 
-\n- pembayaran via ovo, dana, gopay 
-\n================
-\nuntuk join silahkan chat admin👇
+note : 
+- pembayaran via ovo, dana, gopay 
+================
+untuk join silahkan chat admin👇
 admin 1 : <a href="http://wa.me/380995437384">Softmilk</a>
 admin 2 : <a href="http://wa.me/84589340445">Amadeus</a> .
 `;
 
 module.exports.botorder = `<u>Suka sama BOT gini ? mau punya yang sama gini ?</u>
 \nKuy langsung Chat admin untuk Order ataupun untuk source Gratisnya.
-\n================
-\nUntuk Source Gratisnya Bisa Langsung Visit Link Ini : <a href="https://github.com/softmilkpc/MantapjozzSaverGroup">Softmilk</a>
-\nKalau Mau tinggal beres langsung chat admin.
-\n================
-\nOrder Bot silahkan chat admin👇
-\nVia Whatsapp : <a href="http://wa.me/380995437384">Softmilk</a>
+================
+Untuk Source Gratisnya Bisa Langsung Visit Link Ini : <a href="https://github.com/softmilkpc/MantapjozzSaverGroup">Softmilk</a>
+Kalau Mau tinggal beres langsung chat admin.
+================
+Order Bot silahkan chat admin👇
+Via Whatsapp : <a href="http://wa.me/380995437384">Softmilk</a>
 Via Telegram : <a href="http://t.me/softmilkpc">Softmilk PC</a> .
 `;
