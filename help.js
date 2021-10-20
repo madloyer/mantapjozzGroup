635ed57e1824680bc74a63d86690f21975e03911
madloyer <87596107+madloyer@users.noreply.github.com>
2021-10-20 15:32:13 +0700

    Update help.js

--- help.js
+++ help.js
@@ -17,24 +17,24 @@ module.exports.botinstall = `<u>List Join Group Vip PPV / Full Genre Indo, asian
 \nGroup Vip Permanent
 \n==================
 \nGroup Full Indo : 50k
-\nGroup Full Asian : 50k
-\nGroup Full West : 50k
-\nGroup Vip PPV  : 65k
+Group Full Asian : 50k
+Group Full West : 50k
+Group Vip PPV  : 65k
 \nNomor Luar / US : 45k
-\n==================
+==================
 \nGroup Full Indo, Asian , West : 200K
-\nGroup VIP PPV +  Nomor US = 100K
-\n==================
+Group VIP PPV +  Nomor US = 100K
+==================
 \nGroup Full + PPV + Nomor Luar Negeri = 250K
-\n==================
+==================
 \ninfo : untuk Group PPV Media 250k ++ dan butuh Nomor selain +62 untuk akses.
-\n================
+================
 \nnote : 
 \n- pembayaran via ovo, dana, gopay 
 \n================
 \nuntuk join silahkan chat admin👇
-\nadmin 1 : <a href="http://wa.me/380995437384">Softmilk</a>
-\nadmin 2 : <a href="http://wa.me/84589340445">Amadeus</a> .
+admin 1 : <a href="http://wa.me/380995437384">Softmilk</a>
+admin 2 : <a href="http://wa.me/84589340445">Amadeus</a> .
 `;
 
 module.exports.botorder = `<u>Suka sama BOT gini ? mau punya yang sama gini ?</u>
@@ -45,5 +45,5 @@ module.exports.botorder = `<u>Suka sama BOT gini ? mau punya yang sama gini ?</u
 \n================
 \nOrder Bot silahkan chat admin👇
 \nVia Whatsapp : <a href="http://wa.me/380995437384">Softmilk</a>
-\nVia Telegram : <a href="http://t.me/softmilkpc">Softmilk PC</a> .
+Via Telegram : <a href="http://t.me/softmilkpc">Softmilk PC</a> .
 `;
